07a2968733d51f27153fde2b7cca415c043fca95
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-04-17 00:08:26 +0200

    Clasificacion con consultas

--- src/main/java/com/f1data/f1data/api/ClasificacionApi.java
+++ src/main/java/com/f1data/f1data/api/ClasificacionApi.java
@@ -19,7 +19,7 @@ import com.f1data.f1data.service.ClasificacionService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/clasificaion")
+@RequestMapping("/clasificacion")
 public class ClasificacionApi {
   
   @Autowired
